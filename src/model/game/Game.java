676adc387206bc48f5e0f3b190f41f90ec124226
yuval9901 <yuvalgon@post.bgu.ac.il>
2024-08-08 23:22:28 +0300

    Update Game.java
    ff

--- src/model/game/Game.java
+++ src/model/game/Game.java
@@ -29,26 +29,54 @@ public class Game {
         while (player.alive()) {
             cli.display(board.toString());
             cli.displayPlayerStats(player);
-            char action = getPlayerAction();
-            performAction(action);
+            performAction();
             board.gameTick();
         }
         cli.display("Game Over!");
     }
 
-    private char getPlayerAction() {
-        return scanner.next().charAt(0);
+    private String getPlayerAction() {
+        return scanner.next();
     }
 
-    private void performAction(char action) {
-        switch (action) {
-            case 'w': player.moveUp(); break;
-            case 's': player.moveDown(); break;
-            case 'a': player.moveLeft(); break;
-            case 'd': player.moveRight(); break;
-            case 'e': player.castSpecialAbility(); break;
-            case 'q': break; // Do nothing
-            default: System.out.println("Invalid action!"); break;
+    private void performAction() {
+        boolean validAction = false;
+
+        while (!validAction) {
+            String input = getPlayerAction(); // Get input from the player
+            if (input.length() == 1) { // Ensure input is a single character
+                char action = input.charAt(0);
+                switch (action) {
+                    case 'w':
+                        player.moveUp();
+                        validAction = true;
+                        break;
+                    case 's':
+                        player.moveDown();
+                        validAction = true;
+                        break;
+                    case 'a':
+                        player.moveLeft();
+                        validAction = true;
+                        break;
+                    case 'd':
+                        player.moveRight();
+                        validAction = true;
+                        break;
+                    case 'e':
+                        player.castSpecialAbility();
+                        validAction = true;
+                        break;
+                    case 'q':
+                        validAction = true; // Do nothing, but exit loop
+                        break;
+                    default:
+                        System.out.println("Invalid action! Please enter w/a/s/d/e/q.");
+                        break;
+                }
+            } else {
+                System.out.println("Please enter a single character corresponding to an action.");
+            }
         }
     }
 
