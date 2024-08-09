ac3bc6a118204f4521fb7acbbb7cf22e1e7ff532
unknown <amitlavie9@gmail.com>
2024-08-09 13:42:30 +0300

    Added choose playre function

--- src/model/game/Game.java
+++ src/model/game/Game.java
@@ -1,6 +1,6 @@
 package model.game;
 
-import model.tiles.units.players.Player;
+import model.tiles.units.players.*;
 import model.tiles.units.enemies.Enemy;
 import view.CLI;
 import java.util.Scanner;
@@ -12,8 +12,8 @@ import utils.generators.Generator;
 public class Game {
     private Board board;
     private Player player;
-    private CLI cli;
-    private Scanner scanner;
+    private final CLI cli;
+    private final Scanner scanner;
 
     public Game(Board board, Player player) {
         this.board = board;
@@ -25,7 +25,8 @@ public class Game {
         //player.initialize(player.getPosition(), Generator::new, this::handleDeath, this::handleMessage);
     }
 
-    public void start() {
+    public void start()
+    {
         while (player.alive()) {
             cli.display(board.toString());
             cli.displayPlayerStats(player);
@@ -73,6 +74,7 @@ public class Game {
                     default:
                         System.out.println("Invalid action! Please enter w/a/s/d/e/q.");
                         break;
+
                 }
             } else {
                 System.out.println("Please enter a single character corresponding to an action.");
@@ -91,5 +93,34 @@ public class Game {
 
     private void handleMessage(String message) {
         cli.display(message);
+    }
+
+    private void choosePlayer()
+    {
+        int choice = 0;
+        Player[] players = new Player[7];
+        players[0] = new Warrior("Jon Snow",300,30,4,3,this.board);
+        players[1] = new Warrior("The Hound",400,20,6,5,this.board);
+        players[2] = new Mage("Melisandre",100,5,1,300,30,15,5,6,this.board);
+        players[3] = new Mage("Thoros of Myr",250,25,4,150,20,20,3,4,this.board);
+        players[4] = new Rogue("Arya Stark",150,40,2,20,this.board);
+        players[5] = new Rogue("Bronn",250,35,3,50,this.board);
+        players[6] = new Hunter("Ygritte",220,30,2,10,6,this.board);
+
+        this.cli.display("Select player:");
+        for(Player player : players)
+        {
+            this.cli.displayPlayerStats(player);
+        }
+
+        do {
+            while(!scanner.hasNextInt())
+            {
+                scanner.next();
+            }
+            choice = scanner.nextInt();
+        }while(choice < 0 || choice > 6);
+
+        this.player = players[choice];
     }
 }
